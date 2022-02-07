d3a3538db4a1b48f1302d74897e1684391ce8d8a
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-07 12:31:00 -0800

    created links in app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
+
 
 export default function App() {
   return (
     <Router>
       <div className='App'>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="beanie-baby">Find</Link>
+          </li>
+        </ul>
         <Switch>
-          <Route>
+          <Route exact path="/">
             {/* this home page route should list all the beanies */}
+            <BeaniesPage />
           </Route>
           <Route> 
-            {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+            {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>
