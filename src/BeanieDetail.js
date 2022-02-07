9b567f1068d28c7a47ac045896b848686c3e897a
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-07 12:47:48 -0800

    created beaniebabyDetail page

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,25 +1,37 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
+  const [beanieBaby, setBeanieBaby] = useState({});
   // you'll need to get the route param using the appropriate react router hook
+  const params = useParams();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
+    async function onLoad() {
+      const data = await getSingleBeanie(params.id);
 
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+      setBeanieBaby(data);
+    }
 
-  function handleBeanieClick() {
-    // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
-  }
+    onLoad();
+  }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+
+  // function handleBeanieClick() {
+  //   // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
+  // }
 
   return (
-    <>
-      {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
+    <div>
+      <h1>{params.id}</h1>
+      <p>{beanieBaby}</p>
+    </div>
+      /* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */
       
-      {/* <Link to='/'>Home</Link>
+      /* <Link to='/'>Home</Link>
       <div className='beanie-detail' onClick={handleBeanieClick}>
         <div className='beanie-data'>
           <p>{beanieBaby.animal}</p>
@@ -38,7 +50,6 @@ export default function BeanieDetail() {
           <p>Swing Tag Generation: {beanieBaby.swingTagGeneration}</p>
           <p>Tush Tag Generation: {beanieBaby.tushTagGeneration}</p>
         </div>
-      </div>   */}
-    </>
+      </div>   */
   );
 }
