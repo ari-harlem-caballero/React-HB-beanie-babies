292152089aa646de7b46ca904b8df742b5355294
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-07 14:11:27 -0800

    added beanie baby func

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { getSingleBeanie } from './services/fetch-utils';
 
--- src/services/client.js
+++ src/services/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 export const client = createClient(
-  process.env.REACT_SUPABASE_URL,
-  process.env.REACT_SUPABASE_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
 );
 
 export function checkError({ data, error }) {
