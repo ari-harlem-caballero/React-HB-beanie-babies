35abe9875cb0e672c7f4b2ec1e6235688be70469
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-08 08:52:59 -0800

    finished beanies page

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,7 +4,7 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    <Link key={beanieBaby.title} to={`/beanie-baby/${beanieBaby.id}`}>
+    <Link to={`/beanie-baby/${beanieBaby.id}`}>
       <div className='beanie-baby'>
         {beanieBaby.title}
         <img className='beanie-img' src={beanieBaby.image}/>
